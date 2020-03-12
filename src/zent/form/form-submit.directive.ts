6e5a4201b77fa2bc4c182c5bbff97fb47f0130aa
bigmogician <sunaoran@youzan.com>
2020-03-12 14:02:02 +0800

    feat: better submit group

--- src/zent/form/form-submit.directive.ts
+++ src/zent/form/form-submit.directive.ts
@@ -4,6 +4,7 @@ import { IUniversalFormState } from "./form.component";
 
 export enum FormSubmitType {
   Console = "console",
+  Ajax = "ajax",
 }
 
 @Directive({
@@ -15,35 +16,73 @@ export class UniversalFormSubmit extends ZentDirective<IUniversalFormState> {
   @Reference("button")
   protected formSubmitButton!: VariableRef;
 
+  @Reference("group")
+  protected formSubmitGroupId!: VariableRef;
+
   @Reference("submit")
   protected formSubmitId!: VariableRef;
+
+  @Reference("cancel")
+  protected formCancelId!: VariableRef;
 
   @Input({ name: "type", required: true, useEnums: Utils.getEnumValues(FormSubmitType) })
   public formSubmitType: FormSubmitType = FormSubmitType.Console;
 
-  @Input({ name: "text" })
+  @Input({ name: "showCancel" })
+  public formShowCancel!: boolean;
+
+  @Input({ name: "submitText" })
   public formSubmitText!: string;
+
+  @Input({ name: "cancelText" })
+  public formCancelText!: string;
 
   protected async onAttach() {
     const refname = this.render.getRootState("formRefname");
     const childset = this.render.getRootState("formFields");
-    const element = this.createNode("jsx-element")
+    const group = this.createNode("jsx-element")
+      .setTagName(Utils.DOMS.Div)
+      .addJsxAttrs({ key: `"${this.formSubmitGroupId.name}"` });
+    group.addJsxAttr("style", JSON.stringify({ paddingLeft: "60px" }));
+    group.addJsxChild(this.createConfirm(refname));
+    if (this.formShowCancel) {
+      group.addJsxChild(this.createCancel());
+    }
+    childset[this.formSubmitGroupId.name] = {
+      id: this.formSubmitGroupId,
+      element: group,
+    };
+    this.addImports([
+      this.helper.createImport("zent/es/button", this.formSubmitButton.name),
+      this.helper.createImport("zent/css/button.css"),
+    ]);
+  }
+
+  private createCancel() {
+    const cancel = this.createNode("jsx-element")
+      .setTagName(this.formSubmitButton.name)
+      .addJsxAttrs({
+        key: `"${this.formCancelId.name}"`,
+        type: `"${"default"}"`,
+      });
+    cancel.addJsxChild(this.formCancelText ?? "取消");
+    return cancel;
+  }
+
+  private createConfirm(refname: string) {
+    const confirm = this.createNode("jsx-element")
       .setTagName(this.formSubmitButton.name)
       .addJsxAttrs({
         key: `"${this.formSubmitId.name}"`,
         type: `"${"primary"}"`,
       });
     if (this.formSubmitType === FormSubmitType.Console) {
-      element.addJsxAttr("onClick", `() => console.log(${refname}.getValue())`);
+      confirm.addJsxAttr("onClick", `() => console.log(${refname}.getValue())`);
+    } else if (this.formSubmitType === FormSubmitType.Ajax) {
+      // TODO
+      confirm.addJsxAttr("onClick", `() => console.log("TO DO")`);
     }
-    element.addJsxChild(this.formSubmitText || "确认");
-    childset[this.formSubmitButton.name] = {
-      id: this.formSubmitId,
-      element,
-    };
-    this.addImports([
-      this.helper.createImport("zent/es/button", this.formSubmitButton.name),
-      this.helper.createImport("zent/css/button.css"),
-    ]);
+    confirm.addJsxChild(this.formSubmitText ?? "确认");
+    return confirm;
   }
 }
