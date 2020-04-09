815df8fda7e325d9718b9bfc86930da1ef9571b7
bigmogician <sunaoran@youzan.com>
2020-04-09 14:08:37 +0800

    feat: remove zent modules

--- src/composition/composition.module.ts
+++ src/composition/composition.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@amoebajs/builder";
 import { DemoComposition } from "./demo-composition";
-import { LoadingGridComposition } from "./loading-grid";
+// import { LoadingGridComposition } from "./loading-grid";
 
 @Module({
   name: "ambjs-composition-module",
   displayName: "Amoebajs组合模块",
   provider: "react",
-  compositions: [DemoComposition, LoadingGridComposition],
+  compositions: [DemoComposition /* LoadingGridComposition */],
 })
 export class CompositionModule {}
 
-export { DemoComposition, LoadingGridComposition };
+export { DemoComposition /* , LoadingGridComposition */ };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./common/common.module";
 export * from "./layout/layout.module";
 export * from "./composition/composition.module";
-export * from "./zent/zent.module";
